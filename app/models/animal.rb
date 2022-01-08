5c1f19992ad8cb9fb7353c70920de3af8312a0b2
nicolas jambon <nicolas_jambon@hotmail.fr>
2022-01-08 18:08:20 +0100

    Views + New animal avec form + modification specie(s) dans model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,7 +3,7 @@ class Animal < ApplicationRecord
   has_many :bookings
   belongs_to :user
 
-  validates :name, :species, :address, :city, :price_per_day, presence: true
+  validates :name, :specie, :address, :city, :price_per_day, presence: true
   validates :description, presence: true, length: { maximum: 500 }
   validates :name, presence: true, length: { maximum: 50 }
   validates :age, numericality: { only_integer: true }
