e8cfba45ad998ea8d5f31dc6682f2d5cbe4b6ccd
Guillaume Kervran <guillaumekervran@gmail.com>
2022-01-15 16:52:57 +0100

    delete pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit
-
-  private
-
-  def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  end
 end
