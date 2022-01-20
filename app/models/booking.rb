ea5e182055b8dd16107d365bcb2ffb93183f0a44
nicolas jambon <nicolas_jambon@hotmail.fr>
2022-01-20 21:14:16 +0100

    Navbar + routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :animal
   belongs_to :user
 
-  validates :start_date, :end_date, :price_booking, presence: true
+  validates :start_date, :end_date, presence: true
   # assignable_values_for :status, default: 'pending' do
   # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/users/animals', as: 'users_animals', to: 'animals#index'
   resources :animals, shallow: true do
     resources :bookings, only: %i[new create]
     resources :reviews, only: %i[new create]
